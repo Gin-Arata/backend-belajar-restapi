ee15355052e27a4c76aaa82dee8166bd1f302048
Gin-Arata <ginarata62@gmail.com>
2024-09-15 15:16:01 +0700

    add: GET Function with MongoDB

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,5 +1,13 @@
+import connect from "@/lib/db/mongo";
+import User from "@/lib/modals/user";
 import { NextResponse } from "next/server"
 
-export const GET = () => {
-    return new NextResponse("This is my first api.");   
+export const GET = async () => {
+    try {
+        await connect();
+        const user = await User.find();
+        return new NextResponse(JSON.stringify(user));
+    } catch (err: unknown) {
+        return new NextResponse("Error fetching users", { status: 500 });
+    }
 }   
