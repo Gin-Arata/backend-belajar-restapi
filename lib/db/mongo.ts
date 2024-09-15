d5907a73bf1253d029757acf1e9d52525fef6f3e
Gin-Arata <ginarata62@gmail.com>
2024-09-15 15:02:30 +0700

    update: Connection to DB

---
+++ lib/db/mongo.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const connect = async () => {
+  const connectionState = mongoose.connection.readyState;
+
+  if (connectionState === 1) {
+    console.log("MongoDB is already connected");
+    return;
+  } else if (connectionState === 2) {
+    console.log("MongoDB is connecting...");
+    return;
+  }
+
+  try {
+    mongoose.connect(MONGO_URI!, {
+      dbName: "restAPINextJS14",
+      bufferCommands: true,
+    });
+
+    console.log("MongoDB connected successfully");
+  } catch (err: unknown) {
+    console.log("Error connecting to MongoDB", err);
+    throw new Error(`Error connecting to MongoDB: ${err}`);
+  }
+};
+
+export default connect;
